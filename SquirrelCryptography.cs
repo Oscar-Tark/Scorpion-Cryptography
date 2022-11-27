8f19c26ec7489863fcc82c7a89dace4d233b4524
Ferret <oscartark91@protonmail.com>
2022-11-27 18:44:22 +0100

    Changes to unused  Headers

--- SquirrelCryptography.cs
+++ SquirrelCryptography.cs
@@ -15,9 +15,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-using System;
 using System.Text;
-using System.IO;
 using System.Security.Cryptography;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Security;
